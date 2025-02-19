f6bacc1d9dad93dd4c7e02bdab27bff07837a44d
VisBear <SinVisBear9690Gmail.com>
2025-02-19 17:27:20 +0700

    sytle-header

---
+++ src/app/shared/pages/alert/alert.component.ts
@@ -0,0 +1,30 @@
+import { Component, Inject, signal } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-alert',
+  imports: [],
+  templateUrl: './alert.component.html',
+  styleUrl: './alert.component.scss'
+})
+export class AlertComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<AlertComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ){
+    console.log(data, 'data')
+  }
+
+  ngOnInit(){
+
+  }
+
+  cancel(){
+    this.dialogRef.close(false)
+  }
+
+  ok(){
+    this.dialogRef.close(true)
+  }
+}
