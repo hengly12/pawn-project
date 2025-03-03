cdaa4bc3cd058bfd575d9cebbb8622c823e8282c
VisBear <SinVisBear9690Gmail.com>
2025-03-03 17:02:14 +0700

    style and form

--- src/app/components/pawn-form/pawn-form.component.ts
+++ src/app/components/pawn-form/pawn-form.component.ts
@@ -9,10 +9,19 @@ import { MatDatepickerModule} from '@angular/material/datepicker';
 import {MatSelectModule} from '@angular/material/select';
 import { MatIcon } from '@angular/material/icon';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import { GENDER_DATA, ITEM_DATA } from '../../shared/dummy/config';
+import { GENDER_DATA, ITEM_DATA, STATUS_OBJ } from '../../shared/dummy/config';
 import { DataService } from '../../shared/services/data.service';
 import { sign } from 'crypto';
+import { generateKeywords, toDateKey } from '../../shared/services/convert.service';
+import { mapUser } from '../../shared/services/mapping.service';
+import { AuthStore } from '../../auth/auth.store';
 
+interface GenderOption {
+  key: number;
+  text: string;
+  en_name: string;
+
+}
 @Component({
   selector: 'app-pawn-form',
   standalone: true,
@@ -33,7 +42,11 @@ import { sign } from 'crypto';
   styleUrl: './pawn-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
+
 export class PawnFormComponent {
+
+ 
 
   genders = signal<any>(GENDER_DATA);
 
@@ -42,7 +55,9 @@ export class PawnFormComponent {
   seleted = signal<any>(null);
   text = signal<string>('')
   constructor(
-    private ds: DataService, private fb: FormBuilder
+    private ds: DataService, 
+    private fb: FormBuilder,
+    private auth: AuthStore
   ){}
  
 
@@ -52,28 +67,29 @@ export class PawnFormComponent {
   });
 
   pawnForm = new FormGroup({
-    fullName: new FormControl(''),
-    gender: new FormControl(''),
-    phoneNum: new FormControl(''),
-    idCard: new FormControl(''),
+    fullName: new FormControl<any>(null, [Validators.required]),
+    gender: new FormControl<GenderOption | null>(null, Validators.required),
+    phoneNum: new FormControl<number | null>(null,[Validators.required, Validators.pattern('^[0-9]*$')]),
+    idCard: new FormControl<number | null>(null, [Validators.required, Validators.pattern('^[0-9]*$')]),
     address: new FormControl(''),
-    pawnItem: new FormControl(''),
+    pawnItem: new FormControl<any>(null, [Validators.required]),
     description: new FormControl(''),
-    dateCreated: new FormControl(''),
-    dateExpired: new FormControl(''),
-    image: new FormControl(''),
-    phone: new FormControl(''),
-    car: new FormControl(''),
-    phoneId: new FormControl(''),
-    motor:new FormControl(''),
-    jewelry: new FormControl(''),
-    others: new FormControl(''),
-    plateNum: new FormControl(''),
-    brandName: new FormControl(''),
-    pawnPrice: new FormControl(''),
-    monthlyInterest: new FormControl(''),
+    dateCreated: new FormControl<Date | null>(null, Validators.required),
+    dateExpired: new FormControl<Date | null>(null, [Validators.required]),
+    image: new FormControl<string | null>(null),
+    phone: new FormControl<any>(null),
+    car: new FormControl<any>(null),
+    phoneId: new FormControl<any>(null, [Validators.required]),
+    motor:new FormControl<any>(null),
+    jewelry: new FormControl<any>(null),
+    others: new FormControl<any>(null),
+    plateNum: new FormControl<any>(null, [Validators.required]),
+    brandName: new FormControl<any>(null, [Validators.required]),
+    pawnPrice: new FormControl<string | null>(null, [Validators.required]),
+    monthlyInterest: new FormControl<string | null>(null, [Validators.required]),
 
   });
+  
 
   ngOnInit(){
   }
@@ -110,13 +126,32 @@ export class PawnFormComponent {
   }
 
   onSubmit() {
-    console.log(this.pawnForm.value);
+    // if(this.pawnForm.invalid){
+    //   alert('សូមបញ្ចូលព័ត៍មាន');
+    //   return
+    // }
+    const {
+      fullName
+    } = this.pawnForm.getRawValue();
+    const toDay = new Date();
+    
+    const data: any = {
+      key: this.ds.createKey(),
+      created_at: serverTimestamp(),
+      created_by: mapUser(this.auth?.profile),
+      updated_at: serverTimestamp(),
+      updated_by: mapUser(this.auth?.profile),
+      date_key: toDateKey(toDay),
+      status: STATUS_OBJ.ACTIVE,
+      keywords: generateKeywords([fullName]),
+      isDeleted: false,
+
+      full_name: fullName,
+
+    }
+    console.log(data, 'data')
+
+   
+    }
   }
   
-  // ngOnInit(): void {
-  //   this.pawnForm = this.fb.group({
-  //     phone: ['', [Validators.pattern('^[0-9]*$')]] // Allows only numbers
-  //   });
-  // }
-
-}
