d43095f32ee55fdb01ec9c91e04db656c91280e4
VisBear <SinVisBear9690Gmail.com>
2025-10-01 15:41:34 +0700

    notification and fetch data update

--- src/app/components/expired-pawn-dialog/expired-pawn-dialog.component.ts
+++ src/app/components/expired-pawn-dialog/expired-pawn-dialog.component.ts
@@ -1,10 +1,15 @@
 import { Component, Inject, OnInit, signal } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { CommonModule, DatePipe } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 export interface Customer {
   full_name: string;
@@ -14,7 +19,7 @@ export interface Customer {
   price_interest: number;
   created_at?: any;
   gender?: any;
-  pawn_type?: { name?: string, text?: string };
+  pawn_type?: { name?: string; text?: string };
   pawnKey: string;
   pawnCount: number;
   date_expired?: any;
@@ -23,11 +28,17 @@ export interface Customer {
   key: string;
 }
 
-
 @Component({
   selector: 'app-expired-pawn-dialog',
   standalone: true,
-  imports: [MatDialogModule, MatIconModule, MatButtonModule, CommonModule, DatePipe],
+  imports: [
+    MatDialogModule,
+    MatIconModule,
+    MatButtonModule,
+    CommonModule,
+    DatePipe,
+    MatProgressBarModule,
+  ],
   templateUrl: './expired-pawn-dialog.component.html',
   styleUrls: ['./expired-pawn-dialog.component.scss'],
 })
@@ -36,8 +47,15 @@ export class ExpiredPawnDialogComponent implements OnInit {
   formLoading: boolean = false;
   selectedKey: string | undefined;
   param = signal<any>(null);
-  routeUnSubscribe = signal<any>(Subscription);
-  
+
+  isLoading: boolean = false;
+  endOfData: boolean = true;
+  loadingMore: boolean = false;
+
+  pageLimit: number = 6;
+  currentOffset: number = 0;
+  displayedData: Customer[] = [];
+
   constructor(
     public dialogRef: MatDialogRef<ExpiredPawnDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Customer[],
@@ -46,33 +64,79 @@ export class ExpiredPawnDialogComponent implements OnInit {
 
   ngOnInit(): void {
     if (this.data && this.data.length > 0) {
-      this.data.forEach(item => {
-        const expiredDate = item.date_expired?.toDate ? item.date_expired.toDate() : new Date(item.date_expired);
+      this.data.forEach((item) => {
+        const expiredDate = item.date_expired?.toDate
+          ? item.date_expired.toDate()
+          : new Date(item.date_expired);
+        item.date_expired = expiredDate;
         item.isExpired = expiredDate < this.today;
-        
         if (item.pawn_type) {
-          item.displayPawnType = item.pawn_type.name || item.pawn_type.text || 'N/A';
+          item.displayPawnType =
+            item.pawn_type.name || item.pawn_type.text || 'N/A';
         } else {
           item.displayPawnType = 'N/A';
         }
       });
+      this.loadNextChunk();
     }
   }
 
-onCustomerSelect(key: string) {
-  this.formLoading = true;
-  this.selectedKey = key;
-  this.router.navigate([`/home/${this.param()}/listing/create-form/${key}`])
-    .then(() => {
-      this.dialogRef.close(key);
-    })
-    .catch(error => {
-      console.error('Navigation failed:', error);
-    })
-    .finally(() => {
-      this.formLoading = false;
-    });
-}
+  loadNextChunk() {
+    const start = this.currentOffset;
+    const end = start + this.pageLimit;
+
+    const newItems = this.data.slice(start, end);
+
+    if (newItems.length > 0) {
+      this.displayedData = [...this.displayedData, ...newItems];
+      this.currentOffset = end;
+    }
+
+    this.endOfData = this.currentOffset >= this.data.length;
+    this.loadingMore = false;
+    this.isLoading = false;
+  }
+
+  loadMore() {
+    if (this.loadingMore || this.endOfData) return;
+    this.loadingMore = true;
+    if (this.displayedData.length === 0) {
+      this.isLoading = true;
+    }
+
+    setTimeout(() => {
+      this.loadNextChunk();
+    }, 2000);
+  }
+
+  onScroll(event: any) {
+    const element = event.target;
+
+    const threshold = 150;
+
+    const position = element.scrollTop + element.clientHeight;
+    const height = element.scrollHeight;
+
+    if (position > height - threshold) {
+      this.loadMore();
+    }
+  }
+
+  onCustomerSelect(key: string) {
+    this.formLoading = true;
+    this.selectedKey = key;
+    this.router
+      .navigate([`/home/${this.param()}/listing/create-form/${key}`])
+      .then(() => {
+        this.dialogRef.close(key);
+      })
+      .catch((error) => {
+        console.error('Navigation failed:', error);
+      })
+      .finally(() => {
+        this.formLoading = false;
+      });
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
